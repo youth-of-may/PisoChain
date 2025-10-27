75f7084a5a3d030cc1ee1e4368cf77fc6f600cf8
SantosKurtMatthew <kurtsantos924@gmail.com>
2025-10-27 18:36:07 +0800

    Added timestamps to Expense contract. Created ProjectFactory contract.

--- PisoChain.sol
+++ PisoChain.sol
@@ -1,6 +1,37 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.8.2 <0.9.0;
 
+contract Expense {
+    uint public amount;
+    uint public lastUpdatedDate;
+
+    enum Status { PENDING, APPROVED, REJECTED }
+    Status public status;
+
+    /// @notice Initializes a new expense with an amount
+    constructor(uint _amount) {
+        amount = _amount;
+        status = Status.PENDING;
+    }
+
+    /// @notice Sets the expense status to APPROVED
+    function approve() public {
+        status = Status.APPROVED;
+        lastUpdatedDate = block.timestamp;
+    }
+
+    /// @notice Sets the expense status to REJECTED
+    function reject() public {
+        status = Status.REJECTED;
+        lastUpdatedDate = block.timestamp;
+    }
+
+    /// @notice Returns the amount for the expense
+    function getAmount() public view returns (uint) {
+        return amount;
+    }
+}
+
 contract Project {
     Expense[] private projectExpenses;
     Expense[] private approvedExpenses;
@@ -24,6 +55,8 @@ contract Project {
 
     /// @notice Approves the expense and adds the amount the project budget
     function approveExpense(address _expense) public {
+        require(Expense(_expense).status != Status.APPROVED, "Expense already approved");
+
         Expense(_expense).approve();
         projectTotalBudget += Expense(_expense).getAmount();
         approvedExpenses.push(Expense(_expense));
@@ -56,30 +89,24 @@ contract Project {
     }
 }
 
-contract Expense {
-    uint public amount;
+contract ProjectFactory {
+    Project[] private deployedProjects;
 
-    enum Status { PENDING, APPROVED, REJECTED }
-    Status public status;
+    string public factoryName
 
-    /// @notice Initializes a new expense with an amount
-    constructor(uint _amount) {
-        amount = _amount;
-        status = Status.PENDING;
+    /// @notice Initializes the project factory
+    constructor(string memory _factoryName) {
+        factoryName = _factoryName;
     }
 
-    /// @notice Sets the expense status to APPROVED
-    function approve() public {
-        status = Status.APPROVED;
+    /// @notice Initializes a new project
+    function proposeProject(string memory _projectName) public {
+        Project newProject = new Project(_projectName);
+        deployedProjects.push(newProject);
     }
 
-    /// @notice Sets the expense status to REJECTED
-    function reject() public {
-        status = Status.REJECTED;
-    }
-
-    /// @notice Returns the amount for the expense
-    function getAmount() public view returns (uint) {
-        return amount;
+    /// @notice Returns the list of all projects
+    function getAllProjects() public view returns (Project[] memory) {
+        return deployedProjects;
     }
 }
