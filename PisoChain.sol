fbaea017730889f16e245bdd34daf59b9a82c5b8
SantosKurtMatthew <kurtsantos924@gmail.com>
2025-10-27 15:53:15 +0800

    Added getter functions and arrays for approved and rejected expenses.

--- PisoChain.sol
+++ PisoChain.sol
@@ -3,6 +3,8 @@ pragma solidity >=0.8.2 <0.9.0;
 
 contract Project {
     Expense[] private projectExpenses;
+    Expense[] private approvedExpenses;
+    Expense[] private rejectedExpenses;
 
     address private contractor;
     string public projectName;
@@ -13,14 +15,37 @@ contract Project {
         projectName = _projectName;
     }
 
+    function proposeExpense(uint _amount) public {
+        Expense newExpense = new Expense(_amount);
+        projectExpenses.push(newExpense);
+    }
+
     function approveExpense(address _expense) public {
         Expense(_expense).approve();
         projectTotalBudget += Expense(_expense).getAmount();
+        approvedExpenses.push(Expense(_expense));
     }
 
     function rejectExpense(address _expense) public {
         Expense(_expense).reject();
+        rejectedExpenses.push(Expense(_expense));
     } 
+
+    function getAllExpenses() public view returns (Expense[] memory) {
+        return projectExpenses;
+    }
+
+    function getApprovedExpenses() public view returns (Expense[] memory) {
+        return approvedExpenses;
+    }
+
+    function getRejectedExpenses() public view returns (Expense[] memory) {
+        return rejectedExpenses;
+    }
+
+    function getTotalBudget() public view returns (uint) {
+        return projectTotalBudget;
+    }
 }
 
 contract Expense {
