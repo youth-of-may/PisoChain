9c4864cf6d720fc4bba7b4a67e1640a4be9127b8
youth-of-may <princess.giron@obf.ateneo.edu>
2025-10-29 20:32:04 +0800

    Edited styles for dashboard.

--- PisoApp/src/pages/Dashboard.jsx
+++ PisoApp/src/pages/Dashboard.jsx
@@ -5,21 +5,21 @@ export default function Dashboard() {
         
         <h1 className="font-source text-6xl font-bold text-[#1E4E79] mb-4">Every Peso, Every Purpose</h1>
         <p className="w-xl text-center mb-10 font-inter">Lorem ipsum dolor sit amet. Est placeat tenetur ex Quis omnis a tenetur omnis 33 sapiente veritatis est provident galisum ex error odio. </p>
-        <div className="flex justify-around w-full font-inter ">
-            <div className="flex flex-col w-[33%] h-48 bg-[#1f4e7a] text-[#D9D9D9] rounded-xl">
-                <h3>Total Allocated Budget</h3>
-                <h1>₱2.45T</h1>
-                <p>Fiscal Year 2025</p>
+        <div className="flex justify-around w-[95%] font-inter mb-8 gap-x-5">
+            <div className="flex flex-col w-[33%] h-48 bg-[#1f4e7a] text-[#D9D9D9] border-2 rounded-xl justify-center items-start">
+                <h3 className="text-xl ml-4">Total Allocated Budget</h3>
+                <h1 className="text-7xl font-bold font-source text-white ml-4">₱2.45T</h1>
+                <p className="ml-4">Fiscal Year 2025</p>
             </div>
-            <div className="flex flex-col w-[33%] border-2 rounded-xl">
-                <h3 className="text-[#4D4D4D]">Total Allocated Budget</h3>
-                <h1 className="text-[#2C2C2C]">₱2.45T</h1>
-                <p className="text-[#4D4D4D]">Fiscal Year 2025</p>
+            <div className="flex flex-col w-[30%] border-2 rounded-xl justify-center items-start">
+                <h3 className="text-[#4D4D4D] text-xl ml-4">Total Projects</h3>
+                <h1 className="text-[#2C2C2C] text-7xl font-bold font-source ml-4">191,418</h1>
+                <p className="text-[#4D4D4D] ml-4">Fiscal Year 2025</p>
             </div>
-            <div className="flex flex-col w-[33%] border-2 rounded-xl">
-                <h3 className="text-[#4D4D4D]">Total Allocated Budget</h3>
-                <h1 className="text-[#2C2C2C]">₱2.45T</h1>
-                <p className="text-[#4D4D4D]">Fiscal Year 2025</p>
+            <div className="flex flex-col w-[30%] border-2 rounded-xl justify-center items-start">
+                <h3 className="text-[#4D4D4D] text-xl ml-4">Total Approved Expenses</h3>
+                <h1 className="text-[#2C2C2C] text-7xl font-bold font-source ml-4">₱19.5M</h1>
+                <p className="text-[#4D4D4D] ml-4">Fiscal Year 2025</p>
             </div>
         </div>
         <Button>Explore Projects</Button>
