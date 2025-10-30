f9362c21a82686e16351bcffc7574c3f699ab1e8
youth-of-may <princess.giron@obf.ateneo.edu>
2025-10-30 09:22:42 +0800

    Updated link in dashboard.

--- PisoApp/src/pages/Dashboard.jsx
+++ PisoApp/src/pages/Dashboard.jsx
@@ -5,7 +5,7 @@ export default function Dashboard() {
         
         <h1 className="font-source text-6xl font-bold text-[#1E4E79] mb-4">Every Peso, Every Purpose</h1>
         <p className="w-xl text-center mb-10 font-inter">Lorem ipsum dolor sit amet. Est placeat tenetur ex Quis omnis a tenetur omnis 33 sapiente veritatis est provident galisum ex error odio. </p>
-        <div className="flex justify-around w-[95%] font-inter mb-8 gap-x-5">
+        <div className="flex justify-around w-[95%] font-inter mb-16 gap-x-5">
             <div className="flex flex-col w-[33%] h-48 bg-[#1f4e7a] text-[#D9D9D9] border-2 rounded-xl justify-center items-start">
                 <h3 className="text-xl ml-4">Total Allocated Budget</h3>
                 <h1 className="text-7xl font-bold font-source text-white ml-4">₱2.45T</h1>
@@ -22,7 +22,7 @@ export default function Dashboard() {
                 <p className="text-[#4D4D4D] ml-4">Fiscal Year 2025</p>
             </div>
         </div>
-        <Button>Explore Projects</Button>
+        <Button url="/projects" target="_self">EXPLORE PROJECTS</Button>
         </>
     )
 }
