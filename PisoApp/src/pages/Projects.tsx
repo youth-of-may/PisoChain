0ae7affa7502c58a19fde432edafa56d8748e30a
youth-of-may <princess.giron@obf.ateneo.edu>
2025-10-31 16:43:42 +0800

    Created a filter-able list containing projects generated using faker.

--- PisoApp/src/pages/Projects.tsx
+++ PisoApp/src/pages/Projects.tsx
@@ -6,19 +6,35 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
-import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { DualRangeSlider } from '@/components/ui/range';
 import React, { useState } from 'react';
+import { DataTable } from '../components/projects/data-table'
+import { columns, data } from '../components/projects/columns'
+import type { Project } from './components/projects/columns'
+
+async function getData(): Promise<Project[]> {
+  // Fetch data from your API here.
+  return [
+    //sample data
+    {
+      proj_id: 101010,
+      ctr_id: 10000,
+      proj_name: "XOOX",
+      proj_type: "road",
+      proj_desc: "blaaldkalla",
+      proj_status: "pending",
+      proj_loc: "Manila",
+      proj_date: new Date("2025-10-01"),
+      proj_budget: 100000000,
+    },
+    // ...
+  ]
+}
+
 export default function Projects() {
+  //use the code below to access data from blockchain
+  //const data = await getData()
   const [values, setValues] = useState([0, 100000]);
     return(
         <>
@@ -26,28 +42,9 @@ export default function Projects() {
         <p className="w-xl text-center mb-10 font-inter">Lorem ipsum dolor sit amet. Est placeat tenetur ex Quis omnis a tenetur omnis 33 sapiente veritatis est provident galisum ex error odio. </p>
         <div className="w-[75%] flex gap-x-10 justify-center items-center">
             
-          <Input placeholder="Search..."/>
-          <Select>
-          <SelectTrigger className="w-[180px]">
-            <SelectValue placeholder="Progress" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="pending">Pending</SelectItem>
-            <SelectItem value="ongoing">Ongoing</SelectItem>
-            <SelectItem value="completed">Completed</SelectItem>
-          </SelectContent>
-        </Select>
-      <div className="w-[25%] px-10 space-y-2 px-10 flex gap-x-2">
-        Budget
-      <DualRangeSlider
-        label={(value) => value}
-        value={values}
-        onValueChange={setValues}
-        min={0}
-        max={100000000}
-        step={1000}
-      />
-    </div>
+          <div className="container mx-auto flex justify-center">
+        <DataTable columns={columns} data={data} />
+        </div>
           
         </div>
     
