7c6090461b4678684d5bc246457484f0088381ff
youth-of-may <princess.giron@obf.ateneo.edu>
2025-11-02 21:09:02 +0800

    Update main.tsx

--- PisoApp/src/main.tsx
+++ PisoApp/src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter } from "react-router-dom";
 import './index.css'
 import App from './App.js'
-import { Analytics } from "@vercel/analytics/next"
+import { Analytics } from "@vercel/analytics/react"
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
