fd62a48891c61b98945aa3ab30dc284e19e35675
youth-of-may <princess.giron@obf.ateneo.edu>
2025-11-02 18:00:51 +0800

    Added expenses to routes

--- PisoApp/src/App.tsx
+++ PisoApp/src/App.tsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 import MainLayout from './pages/MainLayout';
 import Dashboard from './pages/Dashboard';
-import Regions from "./pages/Regions";
+import Regions from "./pages/Expenses";
 import './index.css'
 import Projects from './pages/Projects';
+import Expenses from "./pages/Expenses";
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Route index element={<Dashboard/>}/>
       <Route path="projects" element={<Projects/>}/>
       <Route path="regions" element={<Regions/>}/>
+      <Route path="projects/:id/expenses" element={<Expenses/>}/>
       </Route>
     </Routes>
       
