2ad9ea58db35b763339c21a4592f467adf700356
youth-of-may <135508335+youth-of-may@users.noreply.github.com>
2025-11-19 14:08:35 +0800

    Changed port

--- server/server.js
+++ server/server.js
@@ -3,10 +3,14 @@ import cors from 'cors';
 import 'dotenv/config';
 import projectRoutes from './api/routes/projects.js';
 import expenseRoutes from './api/routes/expenses.js';
+const PORT = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173', 
+    'https://piso-chain.vercel.app/', 
+  ],
   credentials: true
 }));
 app.use(express.json());
@@ -14,4 +18,4 @@ app.use(express.json());
 app.use('/projects', projectRoutes)
 app.use('/projects', expenseRoutes)
 
-app.listen(5000, () => console.log(`Server running on port 5000`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
