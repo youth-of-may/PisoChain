e62b975e561632f34a8913dc49284b9a22195012
youth-of-may <135508335+youth-of-may@users.noreply.github.com>
2025-12-04 18:16:08 +0800

    Corrected routes in project router

--- server/server.js
+++ server/server.js
@@ -1,19 +1,13 @@
 import express from 'express';
-import projectsRouter from './api/routes/projects.js';
-import expensesRouter from './api/routes/expenses.js';
-
+import projectsRouter from './api/routes/projects.js'
+import expensesRouter from './api/routes/expenses.js'
 const app = express();
-app.use(cors({
-  origin: [
-    'http://localhost:5173', 
-    'https://piso-chain.vercel.app/', 
-  ],
-  credentials: true
-}));
-app.use(express.json());
 
 // Mount the routers with base paths
 app.use('/api/projects', projectsRouter);
 app.use('/api/expenses', expensesRouter);
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
